fafb5c11ca0ffa966fed0b68e2d0eed9bcc26fe2
ThoPN <thopn3@fpt.edu.vn>
2023-01-12 14:50:15 +0700

    OOP

---
+++ Module2_OOP/ProjLib/Person.cs
@@ -0,0 +1,21 @@
+﻿namespace ProjLib
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public DateTime Dob { get; set; }
+
+        internal void InputPerson(int _id, int _name, DateTime _dob)
+        {
+            Id = _id;
+            Name = _name;
+            Dob = _dob;
+        }
+
+        public override string? ToString()
+        {
+            return $"ID: {Id}, Name: {Name}, Dob: {Dob:dd/MM/yyyy}";
+        }
+    }
+}
---
+++ Module2_OOP/Topic2_AccessModifier/Program.cs
@@ -0,0 +1,4 @@
+﻿using ProjLib;
+
+Person p = new Person{ Id = 123, Name = 14, Dob = new DateTime(2003, 10, 12)};
+Console.WriteLine(p);
