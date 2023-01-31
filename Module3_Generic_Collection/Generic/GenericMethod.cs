ffa8b02aab34878aa95c001f0912bb506cfb3f96
ThoPN <thopn3@fpt.edu.vn>
2023-01-31 16:58:00 +0700

    abc

---
+++ Module3_Generic_Collection/Generic/GenericMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    internal class GenericMethod
+    {
+        public void Display<A, B>(A p1, B p2)
+        {
+            Console.WriteLine($"{p1} : {p2}");
+        }
+    }
+}
--- Module3_Generic_Collection/Generic/Program.cs
+++ Module3_Generic_Collection/Generic/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Tạo 1 tập dư liệu vơi kiểu int
+using Generic;
+
+var obj1 = new MyGeneric<int> { Value = 10 };
+Console.WriteLine(obj1);
+
+MyGeneric<string> obj2 = new MyGeneric<string> { Value = "Hello" };
+Console.WriteLine(obj2);
+
+dynamic obj3 = new MyGeneric<dynamic> { Value = new { id = 1, name = "member1" } };
+Console.WriteLine(obj3);
+
+GenericMethod gm = new GenericMethod();
+gm.Display<string, double>("Dung", 10);
+
+
